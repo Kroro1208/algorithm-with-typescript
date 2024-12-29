cf0acf642b9cebe2c965d5c97c265d84e1e5953a
Kroro1208 <yamagata_7580@yahoo.co.jp>
2024-12-29 14:50:21 +0900

    複数条件での検索アルゴリズム実装

--- linear-search/advance.ts
+++ linear-search/advance.ts
@@ -15,3 +15,30 @@ const animals: string[] = ["dog", "cat", "tiger", "lion", "bear", "monkey"];
 
 console.log(linearSearchSentinel(numbers2, 157));
 console.log(linearSearchSentinel(animals, "lion"));
+
+// 複数条件での検索アルゴリズム
+interface User {
+  id: number;
+  name: string;
+  age: number;
+}
+
+// 検索対象となるオブジェクト
+const users: User[] = [
+  { id: 1, name: "Alice", age: 25 },
+  { id: 2, name: "Naoya", age: 38 },
+  { id: 3, name: "Akira", age: 30 },
+  { id: 4, name: "Alice", age: 22 },
+];
+
+// 部分検索可能なようにPartial<User>を使用
+function findUser(users: User[], baseUser: Partial<User>): User[] {
+  return users.filter((user) => {
+    return Object.entries(baseUser).every(
+      ([key, value]) => user[key as keyof User] === value
+    );
+  });
+}
+
+console.log(findUser(users, { name: "Naoya" }));
+console.log(findUser(users, { name: "Alice", age: 44 }));
