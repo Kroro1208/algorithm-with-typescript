4430173ae03d54783c37bb0f43a6933cde8564af
Kroro1208 <yamagata_7580@yahoo.co.jp>
2024-12-31 16:41:32 +0900

    基本的なマージソート実装

--- sort-algorithm/practice.ts
+++ sort-algorithm/practice.ts
@@ -32,3 +32,34 @@ function quickSort<T>(arr: T[]): T[] {
 
 const quickResult = quickSort([64, 34, 25, 12, 22, 11, 90]);
 console.log("Quick Sort:", quickResult);
+
+// 基本的なマージソート
+// 配列を半分に分割し続ける（分割フェーズ）
+function mergeSort<T>(arr: T[]): T[] {
+  if (arr.length <= 1) return arr;
+  const mid = Math.floor(arr.length / 2);
+  const left = mergeSort(arr.slice(0, mid));
+  const right = mergeSort(arr.slice(mid)); // midを含む
+  return merge(left, right);
+}
+
+// 分割した小さな配列をソートしながらマージする（統合フェーズ）
+// 各配列の先頭を比べて、小さい方をresuktに格納
+function merge<T>(left: T[], right: T[]): T[] {
+  const result: T[] = [];
+
+  while (left.length && right.length) {
+    const leftValue = left[0] as T;
+    const rightValue = right[0] as T;
+    if (leftValue <= rightValue) {
+      result.push(left.shift() as T);
+    } else {
+      result.push(right.shift() as T);
+    }
+  }
+
+  return result.concat(left, right);
+}
+
+const mergeResult = mergeSort([64, 34, 25, 12, 22, 11, 90]);
+console.log("Merge Sort:", mergeResult);
