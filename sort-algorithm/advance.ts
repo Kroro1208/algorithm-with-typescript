50ce0f9189b632bfb36204166fdcd783a0baa820
Kroro1208 <yamagata_7580@yahoo.co.jp>
2025-01-01 11:48:19 +0900

    小さい方からk抽出する設定を実装

--- sort-algorithm/advance.ts
+++ sort-algorithm/advance.ts
@@ -22,3 +22,23 @@ const customBubbleResult = bubbleSortCustom(
   (a, b) => a < b
 ); // 左辺が小さかったら入れ替える処理になる
 console.log("降順でソート:", customBubbleResult);
+
+// 配列内の最小のk個の要素を見つけるアルゴリズム
+function findSmaller<T>(arr: T[], k: number): T[] {
+  const result = [...arr];
+  const n = arr.length - 1;
+  for (let i = 0; i < k; i++) {
+    for (let j = 0; j < n - i; j++) {
+      let current = result[j] as T;
+      let next = result[j + 1] as T;
+      if (current < next) {
+        [current, next] = [next, current];
+        result[j] = current;
+        result[j + 1] = next;
+      }
+    }
+  }
+  return result.slice(0, k);
+}
+
+console.log(findSmaller([64, 34, 1, 6, 11, 25, 12, 22, 11, 90], 3));
